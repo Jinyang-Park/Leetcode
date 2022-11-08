e85c640052725a0eee24d1f04223ddf9870c9b95
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-11-08 23:39:03 +0900

    Time: 96 ms (61.93%), Space: 42.3 MB (70.81%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -14,3 +14,17 @@ var runningSum = function(nums) {
     }
 return result
 };
+
+/**
+[1,2,3,4]
+
+1. 첫번째 result에 빈배열 추가
+2. cur = 0으로 지정
+3. for문을 돌면서 nums[0] 즉, 1의 값을 add에 할당한다.
+4. cur += add 는 cur = 0 + 1 이니깐 cur = 1 값이 된다.
+5. result에 cur값을 push하면 [1]값이 들어온다.
+6. 또 for문을 돌면 nums[1] 즉, 2의 값을 add에 할당한다.
+7. cur += add 즉, cur = 1 + 2 이 되서 cur는 3의 값을 할당한다.
+8. result에 cur값을 push하면 [1,3]값이 들어온다.
+9. 이런식으로 포문돌고 할당해주면 [1,3,6,10]이 된다 
+*/
